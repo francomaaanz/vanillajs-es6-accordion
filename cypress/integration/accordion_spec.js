b12160eac40fd9eae2aa2d6663f61da2c39c237d
Franco <francomanz@hotmail.com>
2018-08-23 00:03:17 +0200

    fix del test

--- cypress/integration/accordion_spec.js
+++ cypress/integration/accordion_spec.js
@@ -1,10 +1,10 @@
 describe('My First Test', function() {
     it('Does not do much!', function() {
-      cy.visit('https://francomaaanz.github.io/vanillajs-es6-accordion/dist/')
+      cy.visit('https://francomaaanz.github.io/vanillajs-es6-accordion/')
       cy.get('title').contains('Schibsted Accordion')
       cy.get('#accordion1 > .accordion-panel').should('have.class', 'accordion-panel')
       cy.get('#accordion1:nth-child(1)').click({ force: true, multiple: false })
-      cy.get('#accordion1:first-child').trigger('click').contains('¿Por qué lo usamos?').should('have.class', 'accordion-panel accordion-panel--active')
+      cy.get('#accordion1:nth-child(1)').trigger('click').contains('Ghost Story').should('have.class', 'accordion-panel accordion-panel--active')
       //expect(true).to.equal(true)
 
     })
