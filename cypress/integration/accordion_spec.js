61543d10fbb8ff1b178b6d9ad149fcd943bfcdee
Franco <francomanz@hotmail.com>
2018-08-23 16:54:57 +0200

    ultimos cambios

--- cypress/integration/accordion_spec.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    cy.visit('https://francomaaanz.github.io/vanillajs-es6-accordion/')
-    cy.get('title').contains('Schibsted Accordion')
-    cy.get('#accordion1 > .Accordion-panel').should('have.class', 'Accordion-panel')
-    cy.get('#accordion1:nth-child(1)').click({ force: true, multiple: false })
-    cy.get('#accordion1:nth-child(1)').trigger('click').contains('Ghost Story').should('have.class', 'Accordion-panel is-active')  
-  })
-})
